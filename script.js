f01ddf2f0f24fd4bca9c38a6f5a44cf5aa5de586
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-10-14 20:41:46 +0200

    test

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// books array med litt info
+
 const books =  [
   { 
     name : "The Way Of Shadows", 
@@ -6,7 +8,7 @@ const books =  [
     genre :  [ "Fantary", "Fiction" ],
     characters : [ "Azoth", "Durzo Blint", "Elene Cromwyll", "Jarl", "Logan Gyre", "Rimbold Drake", "Viridiana Sovari", 
                    "Gwinvere Kirena", "Feir Cousat", "Dorian Ursuul", "Solonariwan Tofusin", "Jenine Gyre", "Neph Dada"],
-    language : "english",
+    language : "English",
     published : new date(2008, 10, 1),
     publisher : "Orbit"
   }, 
@@ -17,7 +19,7 @@ const books =  [
     genre :  [ "Fantary", "Fiction" ],
     characters : [ "Azoth", "Durzo Blint", "Elene Cromwyll", "Jarl", "Logan Gyre", "Rimbold Drake", "Viridiana Sovari", 
                    "Gwinvere Kirena", "Feir Cousat", "Dorian Ursuul", "Solonariwan Tofusin", "Jenine Gyre", "Neph Dada"],
-    language : "english",
+    language : "English",
     published : new date(2008, 10, 28),
     pubisher : "Orbit"
 
@@ -27,7 +29,7 @@ const books =  [
     author :  { name : "Brent Weeks", born : new date(1977, 3, 7), location : "Montana U.S" } ,
     series :  { name :"The Night Angel Trioligy", number: 2, seriesCompleted : true, otherBooksInSeries : ["The Way Of Shadows", "Beyond The Shadows"]  } ,
     genre :  [ "Fantary", "Fiction" ],
-    language : "english",
+    language : "English",
     characters : [ "Azoth", "Durzo Blint", "Elene Cromwyll", "Jarl", "Logan Gyre", "Rimbold Drake", "Viridiana Sovari", 
                    "Gwinvere Kirena", "Feir Cousat", "Dorian Ursuul", "Solonariwan Tofusin", "Jenine Gyre", "Neph Dada"],
     published : new date(2008, 11, 25),
@@ -39,41 +41,43 @@ const books =  [
     author : { name: "Robin Hobb", born: new date(1952, 3, 5), location : "Berkley, California U.S", penAs : ["Robin Hobb", "Megan Lindholm" ]} ,
     series : { name : "The Live Ship Traders", number : 1, seriesCompleted: true,  otherBooksInSeries : ["The Mad Ship", "Ship Of Destiny"] } ,
     genre : [ "Fantasy", "Fiction" ],
-    language : "english",
+    language : "English",
     characters : [ "Althea Vestrit", "Ephron Vestrit", "Ronica Vestrit", "Keffria Vestrit", "Kyle Haven", "Wintrow Haven", "Malta Haven", 
                    "Vivacia", "Paragon (Liveship)", "Brashen Trell", "Amber (Liveship Traders)", "Kennit"],
-    published: new date(1998, 3, 2);
+    published: new date(1998, 3, 2),
     publisher : "Bantam"
 
   },
   {
     name : "Dragons Of Winter Night",
     author : [ 
-               { name: "Margaret Weiss", born: new date(1948, 3, 16), location : "Independence, Missouri U.S"}, 
+               { name: "Margaret Weis", born: new date(1948, 3, 16), location : "Independence, Missouri U.S"}, 
                { name: "Tracy Hickman", born: new date(1955, 11, 25), location : "Salt Lake City, Utah U.S"}
              ],
     series : { name : "The DragonLance Saga", number : 2, seriesCompleted: true,  otherBooksInSeries : ["Dragons Of Autumn Twilight", "Dragons Of Spring Dawning"] } ,
     genre : [ "Fantasy", "Fiction", "Epic Fantasy"],
     characters : [ "Tanis Half-Elven", "Sturm Brightblade", "Flint Fireforge", "Tika Waylan", "Caramon Majere", "Raistlin Majere", "Lauralanthalasa", "Kitiara uth Matar", "Tasslehoff Burrfoot", 
                    "Alhana Starbreeze"," Riverwind", "Goldmoon", "kylan Iverson", "Garn", "Aylaen", "Norgaard", "Wulfe", "Raegar", "Gilthanas", "Derek Crownguard"],
-    language : "english",
-    published: new date(1985, 7, 1);
+    language : "English",
+    published: new date(1985, 7, 1),
     publisher : "TSR"
 
   },
   {
     name : "The Serpent Mage ",
-    author : [ { name: "Margaret Weiss", born: new date(1948, 3, 16), location : "Independence, Missouri U.S"}, 
+    author : [ { name: "Margaret Weis", born: new date(1948, 3, 16), location : "Independence, Missouri U.S"}, 
                { name: "Tracy Hickman", born: new date(1955, 11, 25), location : "Salt Lake City, Utah U.S"}  ],
     series : { name : "The Death Gate Cycle", number : 4, seriesCompleted: true, othersInSeries : ["Dragon Wing", "Elven Star", " Fire Sea", "The Hand Of Chaos", "Into The Labyrinth", "The Seventh Gate"] } ,
     genre : [ "Fantasy", "Fiction", "Epic Fantasy"],
-    language : "english",
+    language : "English",
     characters : ["Haplo", "Alfred", "Devon", "Grundle", "Alake", "Sang-Drax"],
     published: new date(1992, 1, 1),
     pubisher : "Bantam"
 
   }
 ]; 
+
+// publishers array - skal kobles mot publishers key i books for å hindre dobbel registrering
 
 const publishers = [
   {
@@ -89,7 +93,7 @@ const publishers = [
     fullName : "Batam Books",
     founded : new date(1945),
     countryOfOrigin : "United States",
-    fouuders :  ["Walter B. Pitkin, Jr." , "Sidney B. Kramer"," Ian Ballantine","Betty Ballantine"] ,
+    founders :  ["Walter B. Pitkin, Jr." , "Sidney B. Kramer"," Ian Ballantine","Betty Ballantine"] ,
     webPage : "https://www.randomhousebooks.com/imprint/bantam-books/"
   },
   { 
@@ -97,9 +101,9 @@ const publishers = [
     fullName : "TSR Inc. (now : Wizards Of The Coast)",
     founded : new date(1983),
     countryOfOrigin : "United States",
-    fouuders :  ["Gary Gygax", "Don Kaye"],
+    founders :  ["Gary Gygax", "Don Kaye"],
     webPage : "https://company.wizards.com/en"
-  },
+  }
   
 ];
 

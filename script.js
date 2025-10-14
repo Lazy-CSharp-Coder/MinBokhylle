5377e87035c4b8558a0b39624efdd493bef36541
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-10-14 17:16:01 +0200

    test

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ const books =  [
   { 
     name : "The Way Of Shadows", 
     author :  { name : "Brent Weeks", born : new date(1977, 3, 7), location : "Montana U.S" } ,
-    series :  { name :"The Night Angel Trioligy", number: 1, seriesCompleted : true } ,
+    series :  { name :"The Night Angel Trioligy", number: 1, seriesCompleted : true, otherBooksInSeries : ["Beyond The Shadows", "Shadows Edge"] } ,
     genre :  [ "Fantary", "Fiction" ],
+    characters : [ "Azoth", "Durzo Blint", "Elene Cromwyll", "Jarl", "Logan Gyre", "Rimbold Drake", "Viridiana Sovari", 
+                   "Gwinvere Kirena", "Feir Cousat", "Dorian Ursuul", "Solonariwan Tofusin", "Jenine Gyre", "Neph Dada"],
     language : "english",
     published : new date(2008, 10, 1)
   }, 
   {
     name : "Beyond The Shadows",
     author : { name : "Brent Weeks", born : new date(1977, 3, 7), location : "Montana U.S" } ,
-    series : { name :"The Night Angel Trioligy", number: 2, seriesCompleted : true } ,
+    series : { name :"The Night Angel Trioligy", number: 2, seriesCompleted : true, otherBooksInSeries : ["The Way Of Shadows", "Shadows Edge"] } ,
     genre :  [ "Fantary", "Fiction" ],
+    characters : [ "Azoth", "Durzo Blint", "Elene Cromwyll", "Jarl", "Logan Gyre", "Rimbold Drake", "Viridiana Sovari", 
+                   "Gwinvere Kirena", "Feir Cousat", "Dorian Ursuul", "Solonariwan Tofusin", "Jenine Gyre", "Neph Dada"],
     language : "english",
     published : new date(2008, 10, 28)
 
@@ -19,40 +23,48 @@ const books =  [
   {
     name : "Shadows Edge",
     author :  { name : "Brent Weeks", born : new date(1977, 3, 7), location : "Montana U.S" } ,
-    series :  { name :"The Night Angel Trioligy", number: 2, seriesCompleted : true } ,
+    series :  { name :"The Night Angel Trioligy", number: 2, seriesCompleted : true, otherBooksInSeries : ["The Way Of Shadows", "Beyond The Shadows"]  } ,
     genre :  [ "Fantary", "Fiction" ],
     language : "english",
+    characters : [ "Azoth", "Durzo Blint", "Elene Cromwyll", "Jarl", "Logan Gyre", "Rimbold Drake", "Viridiana Sovari", 
+                   "Gwinvere Kirena", "Feir Cousat", "Dorian Ursuul", "Solonariwan Tofusin", "Jenine Gyre", "Neph Dada"],
     published : new date(2008, 11, 25)
 
   },
   {
     name : "Ship Of Magic",
-    author : { name: "Robin Hobb", born: new date(1952, 3, 5), location : "Berkley, California U.S", penAlias : "Megan Lindholm"} ,
-    series : { name : "The Live Ship Traders", number : 1, seriesCompleted: true} ,
+    author : { name: "Robin Hobb", born: new date(1952, 3, 5), location : "Berkley, California U.S", penAs : ["Robin Hobb", "Megan Lindholm" ]} ,
+    series : { name : "The Live Ship Traders", number : 1, seriesCompleted: true,  otherBooksInSeries : ["The Mad Ship", "Ship Of Destiny"] } ,
     genre : [ "Fantasy", "Fiction" ],
     language : "english",
+    characters : [ "Althea Vestrit", "Ephron Vestrit", "Ronica Vestrit", "Keffria Vestrit", "Kyle Haven", "Wintrow Haven", "Malta Haven", 
+                   "Vivacia", "Paragon (Liveship)", "Brashen Trell", "Amber (Liveship Traders)", "Kennit"],
     published: new date(1998, 3, 2);
 
   },
   {
-    name : "Dragons Of Autumn Twilight",
+    name : "Dragons Of Winter Night",
     author : [ 
                { name: "Margaret Weiss", born: new date(1948, 3, 16), location : "Independence, Missouri U.S"}, 
                { name: "Tracy Hickman", born: new date(1955, 11, 25), location : "Salt Lake City, Utah U.S"}
              ],
-    series : { name : "The DragonLance Saga", number : 1, seriesCompleted: true} ,
-    genre : [ "Fantasy", "Fiction" ],
+    series : { name : "The DragonLance Saga", number : 2, seriesCompleted: true,  otherBooksInSeries : ["Dragons Of Autumn Twilight", "Dragons Of Spring Dawning"] } ,
+    genre : [ "Fantasy", "Fiction", "Epic Fantasy"],
+    characters : [ "Tanis Half-Elven", "Sturm Brightblade", "Flint Fireforge", "Tika Waylan", "Caramon Majere", "Raistlin Majere", "Lauralanthalasa", "Kitiara uth Matar", "Tasslehoff Burrfoot", 
+                   "Alhana Starbreeze"," Riverwind", "Goldmoon", "kylan Iverson", "Garn", "Aylaen", "Norgaard", "Wulfe", "Raegar", "Gilthanas", "Derek Crownguard"],
     language : "english",
-    published: new date(1998, 3, 2);
+    published: new date(1985, 7, 1);
 
   },
   {
-    name : "Ship Of Magic",
-    author : [ { name: "Robin Hobb", born: new date(1952, 3, 5), location : "Berkley, California U.S", penAlias : "Megan Lindholm"} ],
-    series : [ { name : "The Live Ship Traders", number : 1, seriesCompleted: true} ],
-    genre : [ "Fantasy", "Fiction" ],
+    name : "The Serpent Mage ",
+    author : [ { name: "Margaret Weiss", born: new date(1948, 3, 16), location : "Independence, Missouri U.S"}, 
+               { name: "Tracy Hickman", born: new date(1955, 11, 25), location : "Salt Lake City, Utah U.S"}  ],
+    series : { name : "The Death Gate Cycle", number : 4, seriesCompleted: true, othersInSeries : ["Dragon Wing", "Elven Star", "Fire Sea", "The Hand Of Chaos", "Into The Labyrinth", "The Seventh Gate"] } ,
+    genre : [ "Fantasy", "Fiction", "Epic Fantasy"],
     language : "english",
-    published: new date(1998, 3, 2);
+    characters : ["Haplo", "Alfred", "Devon", "Grundle", "Alake", "Sang-Drax"],
+    published: new date(1992, 1, 1);
 
   }
 ]; 

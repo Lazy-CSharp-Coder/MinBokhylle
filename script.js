ef8ea46d30da2914836662e29be0eaae6de18dee
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-10-20 10:52:32 +0200

    test

--- script.js
+++ script.js
@@ -161,6 +161,28 @@ maleCheck.addEventListener("change", () => { maleCheck.checked = true; femaleChe
 
 enterBookshelfButton.addEventListener("click", enterDatabase);
 
+// sette opp klokken
+
+const clockDiv = document.querySelector("#clockDiv");
+
+function updateClock()
+{
+   const timeNow = new Date;
+   let hours = timeNow.getHours();
+   let minutes = timeNow.getMinutes();
+   let seconds = timeNow.getSeconds();
+
+   if(hours < 10) hours = "0" + hours;
+   if(minutes < 10) minutes = "0" + minutes;
+   if(seconds < 10) seconds = "0" + seconds;
+
+   const timeString = `${hours}:${minutes}:${seconds}`;
+   clockDiv.textContent = timeString;
+}
+
+
+
+
 function introducingBookshelf()
 {
    if(femaleCheck.checked) aiSelected = "Adeline";

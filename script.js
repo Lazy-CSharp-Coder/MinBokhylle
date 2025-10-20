321158248279c21977c771562209fa8129401cb8
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-10-20 14:55:31 +0200

    test

--- script.js
+++ script.js
@@ -124,51 +124,53 @@ const bookDatabase =
 
 const maleEffects = 
 {
-   enterListItems : new Audio("/Sounds/enterlistitems.mp3"),
-   accessingBooks : new Audio("/Sounds/accessing.mp3"),
-   accessing : new Audio("/Sounds/accessing.mp3"),
-   enterSearchMode : new Audio("/Sounds/searchmode.mp3"),
-   switching : new Audio("/Sounds/accessing.mp3"),
-   bookIntro : new Audio("/Sounds/bookintro.mp3"),
-   featureNotAvailable : new Audio("/Sounds/notavailable.mp3"),
-   alreadyInSearchMode : new Audio("/Sounds/alreadysearching.mp3"),
-   requestNotFound : new Audio("/Sounds/requestnotfound.mp3"),
-   noEntriesFound : new Audio("/Sounds/noentriesfound.mp3"),
-   clickListItem : new Audio("/Sounds/clicklistitem.mp3"),
-   iconsCreatedBy : new Audio("/Sounds/iconscreatedby.mp3"),
-   lightMode : new Audio("/Sounds/lightmode.mp3"),
-   darkMode : new Audio("/Sounds/darkmode.mp3"),
-   morning : new Audio("/Sounds/accessing.mp3"),
-   afternoon : new Audio("/Sounds/iconscreatedby.mp3"),
-   evening : new Audio("/Sounds/noentriesfound.mp3"),
-   night : new Audio("/Sounds/searchmode.mp3"),
-   introduction : new Audio("/Sounds/searchmode.mp3"),
-   maintenance : new Audio("/Sounds/maintenace.mp3")
+   enterListItems : new Audio("/Sounds/enterlistitemsj.mp3"),
+   accessingBooks : new Audio("/Sounds/accessingj.mp3"),
+   accessing : new Audio("/Sounds/accessingj.mp3"),
+   enterSearchMode : new Audio("/Sounds/searchmodej.mp3"),
+   switching : new Audio("/Sounds/switchingj.mp3"),
+   bookIntro : new Audio("/Sounds/bookintroj.mp3"),
+   featureNotAvailable : new Audio("/Sounds/notavailablej.mp3"),
+   alreadyInSearchMode : new Audio("/Sounds/alreadysearchingj.mp3"),
+   requestNotFound : new Audio("/Sounds/requestnotfoundj.mp3"),
+   noEntriesFound : new Audio("/Sounds/noentriesfoundj.mp3"),
+   clickListItem : new Audio("/Sounds/clicklistitemj.mp3"),
+   iconsCreatedBy : new Audio("/Sounds/iconscreatedbyj.mp3"),
+   lightMode : new Audio("/Sounds/lightmodej.mp3"),
+   darkMode : new Audio("/Sounds/darkmodej.mp3"),
+
+   morning : new Audio("/Sounds/morningj.mp3"),
+   afternoon : new Audio("/Sounds/afternoonej.mp3"),
+   evening : new Audio("/Sounds/eveningj.mp3"),
+   night : new Audio("/Sounds/nightj.mp3"),
+   introduction : new Audio("/Sounds/introj.mp3"),
+   maintenance : new Audio("/Sounds/maintenacej.mp3")
 
 }
 
 const femaleEffects = 
 {
-   enterListItems : new Audio("/Sounds/enterlistitems.mp3"),
-   accessingBooks : new Audio("/Sounds/accessing.mp3"),
-   accessing : new Audio("/Sounds/accessing.mp3"),
-   enterSearchMode : new Audio("/Sounds/searchmode.mp3"),
-   switching : new Audio("/Sounds/notavailable.mp3"),
-   bookIntro : new Audio("/Sounds/bookintro.mp3"),
-   featureNotAvailable : new Audio("/Sounds/notavailable.mp3"),
-   alreadyInSearchMode : new Audio("/Sounds/alreadysearching.mp3"),
-   requestNotFound : new Audio("/Sounds/requestnotfound.mp3"),
-   noEntriesFound : new Audio("/Sounds/noentriesfound.mp3"),
-   clickListItem : new Audio("/Sounds/clicklistitem.mp3"),
-   iconsCreatedBy : new Audio("/Sounds/iconscreatedby.mp3"),
-   lightMode : new Audio("/Sounds/lightmode.mp3"),
-   darkMode : new Audio("/Sounds/darkmode.mp3"),
-   morning : new Audio("/Sounds/accessing.mp3"),
-   afternoon : new Audio("/Sounds/iconscreatedby.mp3"),
-   evening : new Audio("/Sounds/noentriesfound.mp3"),
-   night : new Audio("/Sounds/searchmode.mp3"),
-   introduction : new Audio("/Sounds/searchmode.mp3"),
-   maintenance : new Audio("/Sounds/maintenace.mp3")
+   enterListItems : new Audio("/Sounds/enterlistitemsa.mp3"),
+   accessingBooks : new Audio("/Sounds/accessinga.mp3"),
+   accessing : new Audio("/Sounds/accessinga.mp3"),
+   enterSearchMode : new Audio("/Sounds/searchmodea.mp3"),
+   switching : new Audio("/Sounds/notavailablea.mp3"),
+   bookIntro : new Audio("/Sounds/switchinga.mp3"),
+   featureNotAvailable : new Audio("/Sounds/notavailablea.mp3"),
+   alreadyInSearchMode : new Audio("/Sounds/alreadysearchinga.mp3"),
+   requestNotFound : new Audio("/Sounds/requestnotfounda.mp3"),
+   noEntriesFound : new Audio("/Sounds/noentriesfounda.mp3"),
+   clickListItem : new Audio("/Sounds/clicklistitema.mp3"),
+   iconsCreatedBy : new Audio("/Sounds/iconscreatedbya.mp3"),
+   lightMode : new Audio("/Sounds/lightmodea.mp3"),
+   darkMode : new Audio("/Sounds/darkmodea.mp3"),
+
+   morning : new Audio("/Sounds/morninga.mp3"),
+   afternoon : new Audio("/Sounds/afternoona.mp3"),
+   evening : new Audio("/Sounds/eveninga.mp3"),
+   night : new Audio("/Sounds/nighta.mp3"),
+   introduction : new Audio("/Sounds/introa.mp3"),
+   maintenance : new Audio("/Sounds/maintenacea.mp3")
 
 }
 

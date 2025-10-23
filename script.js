c3975bcfd1af645d523b5eb7a02ad7f68e5a679f
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-10-23 11:08:26 +0200

    test

--- script.js
+++ script.js
@@ -1,67 +1,6 @@
 // books array med litt info
 import { bookDatabase } from "./js/bookDatabase.js"; 
-
-const maleEffects = 
-{
-   aiName : "John",
-   enterListItems : new Audio("/Sounds/enterlistitemsj.mp3"),
-   accessingBooks : new Audio("/Sounds/accessingj.mp3"),
-   accessing : new Audio("/Sounds/accessingj.mp3"),
-   enterSearchMode : new Audio("/Sounds/searchmodej.mp3"),
-   switching : new Audio("/Sounds/switchingj.mp3"),
-   bookIntro : new Audio("/Sounds/bookintroj.mp3"),
-   featureNotAvailable : new Audio("/Sounds/notavailablej.mp3"),
-   alreadyInSearchMode : new Audio("/Sounds/alreadysearchingj.mp3"),
-   requestNotFound : new Audio("/Sounds/requestnotfoundj.mp3"),
-   noEntriesFound : new Audio("/Sounds/noentriesfoundj.mp3"),
-   clickListItem : new Audio("/Sounds/clicklistitemj.mp3"),
-   iconsCreatedBy : new Audio("/Sounds/iconscreatedbyj.mp3"),
-   lightMode : new Audio("/Sounds/lightmodej.mp3"),
-   darkMode : new Audio("/Sounds/darkmodej.mp3"),
-
-   morning : new Audio("/Sounds/morningj.mp3"),
-   afternoon : new Audio("/Sounds/afternoonj.mp3"),
-   evening : new Audio("/Sounds/eveningj.mp3"),
-   night : new Audio("/Sounds/nightj.mp3"),
-   introduction : new Audio("/Sounds/introj.mp3"),
-   maintenance : new Audio("/Sounds/maintenancej.mp3"),
-   website : new Audio("/Sounds/webpagej.mp3"),
-
-   activate : new Audio("/Sounds/activatej.mp3"),
-   retrieving : new Audio("/Sounds/retrievingj.mp3")
-
-}
-
-const femaleEffects = 
-{
-   aiName : "Adeline",
-   enterListItems : new Audio("/Sounds/enterlistitemsa.mp3"),
-   accessingBooks : new Audio("/Sounds/accessinga.mp3"),
-   accessing : new Audio("/Sounds/accessinga.mp3"),
-   enterSearchMode : new Audio("/Sounds/searchmodea.mp3"),
-   switching : new Audio("/Sounds/switchinga.mp3"),
-   bookIntro : new Audio("/Sounds/bookintroa.mp3"),
-   featureNotAvailable : new Audio("/Sounds/notavailablea.mp3"),
-   alreadyInSearchMode : new Audio("/Sounds/alreadysearchinga.mp3"),
-   requestNotFound : new Audio("/Sounds/requestnotfounda.mp3"),
-   noEntriesFound : new Audio("/Sounds/noentriesfounda.mp3"),
-   clickListItem : new Audio("/Sounds/clicklistitema.mp3"),
-   iconsCreatedBy : new Audio("/Sounds/iconscreatedbya.mp3"),
-   lightMode : new Audio("/Sounds/lightmodea.mp3"),
-   darkMode : new Audio("/Sounds/darkmodea.mp3"),
-
-   morning : new Audio("/Sounds/morninga.mp3"),
-   afternoon : new Audio("/Sounds/afternoona.mp3"),
-   evening : new Audio("/Sounds/eveninga.mp3"),
-   night : new Audio("/Sounds/nighta.mp3"),
-   introduction : new Audio("/Sounds/introa.mp3"),
-   maintenance : new Audio("/Sounds/maintenancea.mp3"),
-   website : new Audio("/Sounds/webpagea.mp3"),
-
-   activate : new Audio("/Sounds/activatea.mp3"),
-   retrieving : new Audio("/Sounds/retrievinga.mp3")
-
-}
+import { maleEffects, femaleEffects } from "./js/soundEffects.js";
 
 // startpage funsjoner og eventlisteners 
 
